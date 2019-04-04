c6e4a1559f39beeaf2977a2e20bc055db9e8d128
Axelm3sh <axel.m3sh@gmail.com>
2019-04-04 11:21:00 -0700

    Universal click event (at least within PageManager.js)
    Give an element the data-click attribute followed by the function name you want to call
    
    ie: a button element would have
    <button data-click="myFunc"></button>
    
    and in JS, implement
    clickActions["myFunc"] = function(e){
    //do stuff
    }

--- Scripts/PageManager.js
+++ Scripts/PageManager.js
@@ -1,16 +1,46 @@
 //$-prefix variables are Jquery objects, you can use this variable to do Jquery functions on the doc
-var $docObj = $(document);
+var $docObj = $(document.body);
 var $WeekdayContainer = $("#WeekdayContainer");
 var defaultColorAR = ["#3e9ce9", "#e98b3e", "#00c4b1", "#5d65ef", "#a165ed", "#ea63b0", "#e9593e"];
 var complementColorAR = ["#97cdf4", "#f6c555", "#00dcd0", "#a3a6f6", "#bd94f1", "#ed91c7", "#e8a09c"];
 
 
+//universal click handler functionality, 
+//just add a: data-click="actionName" attribute to the element.
+var clickActions = {};
+//catch all click events
+$docObj.on("click", "*", function(elem) {
+    var $this = $(this);
+    
+    var actionName = $this.data("click") || false;
+    
+    if(typeof clickActions[actionName] === 'function')
+        {
+            clickActions[actionName].call(this, elem);
+        }
+});
+
+//example, look at index.html nav notification button
+clickActions["notification"] = function (e)
+{
+    alert("You clicked on notifications!");
+}
+
+
+//JQUERY ready is similar to window.onload except it occurs earlier
 $docObj.ready(function () {
     console.log("Page has loaded!");
     //    alert("Page loaded, welcome!");
     loadTemplate();
     
 });
+
+//default DOM event occurs when images and everything is loaded
+window.onload()
+{
+    postColorFix();
+}
+
 
 function loadTemplate() {
     //TODO: dynamically load in html templates for vertical day slice
