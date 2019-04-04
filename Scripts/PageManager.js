65678f041feacea46b04adec164a1adf0aa01864
Axelm3sh <axel.m3sh@gmail.com>
2019-04-04 16:15:36 -0700

    Fixed and Optimized template generation calls
    +fixed colors, now nicer looking
    +fadeInElement function replaced with direct call, might refactor for use later tho
    +prototype column expand, need to add animation and fix css

--- Scripts/PageManager.js
+++ Scripts/PageManager.js
@@ -1,45 +1,70 @@
 //$-prefix variables are Jquery objects, you can use this variable to do Jquery functions on the doc
 var $docObj = $(document.body);
 var $WeekdayContainer = $("#WeekdayContainer");
-var defaultColorAR = ["#3e9ce9", "#e98b3e", "#00c4b1", "#5d65ef", "#a165ed", "#ea63b0", "#e9593e"];
-var complementColorAR = ["#97cdf4", "#f6c555", "#00dcd0", "#a3a6f6", "#bd94f1", "#ed91c7", "#e8a09c"];
+var defaultColorAR = ["#3e9ce9", "#e98b3e", "#14d19e", "#e9593e", "#5d65ef", "#a81fff", "#ea63b0"];
+var complementColorAR = ["#97cdf4", "#f6c555", "#99f299", "#e8a09c", "#a3a6f6", "#db85ff", "#ed91c7"];
 
 
 //universal click handler functionality, 
 //just add a: data-click="actionName" attribute to the element.
 var clickActions = {};
 //catch all click events
-$docObj.on("click", "*", function(elem) {
+$docObj.on("click", "*", function (elem) {
     var $this = $(this);
-    
+
     var actionName = $this.data("click") || false;
-    
-    if(typeof clickActions[actionName] === 'function')
-        {
-            clickActions[actionName].call(this, elem);
-        }
+
+    if (typeof clickActions[actionName] === 'function') {
+        clickActions[actionName].call(this, elem);
+    }
 });
 
 //example, look at index.html nav notification button
-clickActions["notification"] = function (e)
-{
+clickActions["notification"] = function (e) {
     alert("You clicked on notifications!");
 }
 
+
+clickActions["daySlot"] = function (e) {
+    console.log("day slot clicked: " + e.currentTarget);
+    
+    //make JQuery obj from given element
+    var $target = $(e.currentTarget) || $();
+
+    //Shrink everything to smaller size
+    $WeekdayContainer.find(".colorFrameBase").each(function (elem) {
+        $(this).addClass("shrinkCol");
+    });
+
+    //Check to see if we should expand or revert to normal
+    if ($target.hasClass("expandedCol")) 
+    {
+        //revert everything to default
+        $WeekdayContainer.find(".colorFrameBase").each(function (elem) {
+            $(this).removeClass("shrinkCol");
+            $(this).removeClass("expandedCol");
+        });
+    }
+    else //convert single clicked row to expanded view
+    { 
+        $target.removeClass("shrinkCol");
+        $target.addClass("expandedCol");
+    }
+
+}
 
 //JQUERY ready is similar to window.onload except it occurs earlier
 $docObj.ready(function () {
     console.log("Page has loaded!");
     //    alert("Page loaded, welcome!");
     loadTemplate();
-    
+
 });
 
 //default DOM event occurs when images and everything is loaded
-window.onload()
-{
-    postColorFix();
-}
+window.onload = function () {
+    //postColorFix();
+};
 
 
 function loadTemplate() {
@@ -47,10 +72,14 @@ function loadTemplate() {
     //Use jquery.load( "path/file.html #toSelector");
     //$WeekdayContainer.load("HTML/Template01.html");
 
+    //Load template once
+    var rawHtml = $("#Template01").text();
+
+
     //Append divs with IDs containing daySlot followed by #
     for (var i = 1; i <= 7; i++) {
         //generated divs
-        var newHTML = "<div class=\"colorFrameBase card\" $></div>";
+        var newHTML = "<div class=\"colorFrameBase card\" $ data-click=\"daySlot\"></div>";
         //ex: should be id="daySlot1"
         newHTML = newHTML.replace("$", "id=\"daySlot" + i + "\"");
 
@@ -59,30 +88,51 @@ function loadTemplate() {
 
         //each day slot gets loaded up with a template
         var $tempDaySlot = $("#daySlot" + i);
-        //For now we can directly change the entire slot container's collr
-        $tempDaySlot.css("background-color", defaultColorAR[i - 1]);
-        //Load 
-        $tempDaySlot.load("HTML/Template01.html", fadeInElement($tempDaySlot));
-        $tempDaySlot.css("zIndex", -i);
+
+        //Load// Edit: 7 calls to server doesn't make sense, why not call
+        //once outside loop and reuse template?
+        //         $tempDaySlot.load("HTML/Template01.html", 
+        //         fadeInElement($tempDaySlot));
+
+        //Alternative load, template from script, select from script ID
+        //        var rawHtml = $("#Template01").text();
+        //        $tempDaySlot.html(rawHtml);
+
+        //Alternative to fadeInElement function, direct chain callback
+        $tempDaySlot.html(rawHtml).hide().fadeIn("slow", function () {
+            //            postColorFix();
+
+        });
+
+
+
+        $tempDaySlot.css("zIndex", 8 - i);
     }
+
+    postColorFix();
 
 }
 
 //Quick visual for "loading in" the app
 function fadeInElement(param) {
+    //IDE intellisense tip, function was expecting a Jquery object but 
+    //we can trick it into thinking it's a Jquery object by setting it to itself OR empty Jquery object (can be extended to any object)
+    param = param || $();
+
     param.hide();
     param.fadeIn("slow");
     postColorFix();
 }
 
-//~Not working, template being loaded out of order or there's a delay between load and callback~
+//Fixes the colors of the frames once it loads
+//Maybe add a color selector thing down the line or parse colors from input
 function postColorFix() {
 
     $WeekdayContainer.find(".colorFrameTop").each(function (index) {
-        $(this).css("background-color", defaultColorAR[index - 1]);
+        $(this).css("background-color", defaultColorAR[index]);
     });
 
     $WeekdayContainer.find(".colorFrameContent").each(function (index) {
-        $(this).css("background-color", complementColorAR[index - 1]);
+        $(this).css("background-color", complementColorAR[index]);
     });
 }
