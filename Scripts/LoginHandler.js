7575212dcab70afa1dea014109c5ab09fa2c18bf
Axelm3sh <axel.m3sh@gmail.com>
2019-04-17 16:16:41 -0700

    Got Authentication to display the popup
    Need to refactor the page to allow sign in redirect and other displaying functions...

--- Scripts/LoginHandler.js
+++ Scripts/LoginHandler.js
@@ -1,22 +1,25 @@
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().languageCode = 'pt';
-
-
-
 //default DOM event occurs when images and everything is loaded
 window.onload = function () {
 
 };
 
 function GAuthPopup() {
+
+    console.log("Running Google Authentication");
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().useDeviceLanguage();
+    provider.addScope("profile");
+    provider.addScope("email");
+
     firebase.auth().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         // ...
-        
-        
+        alert("successful login");
+
     }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -26,5 +29,7 @@ function GAuthPopup() {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
+
+        alert(errorCode + errorMessage);
     });
 }
---
+++ Scripts/init-firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCJj6GEo-xQFRV5tXDBKVXBcAeM11oLKpo",
+    authDomain: "tworkerplanner.firebaseapp.com",
+    databaseURL: "https://tworkerplanner.firebaseio.com",
+    projectId: "tworkerplanner",
+    storageBucket: "tworkerplanner.appspot.com",
+    messagingSenderId: "495053764521"
+};
+
+firebase.initializeApp(config);
